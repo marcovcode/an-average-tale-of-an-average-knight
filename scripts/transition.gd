11ab1231bb612e9d89e69f489de58f3f1ea10230
marcovcode <marcov.code@gmail.com>
2024-12-08 22:07:27 +0100

    Better layer names

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -2,20 +2,20 @@ extends CanvasLayer
 
 signal on_transition_finished
 
-@onready var color_rect = $ColorRect
+@onready var panel = $Panel
 @onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
-    color_rect.visible = false
-    animation_player.animation_finished.connect(_on_animation_finished)
+	panel.visible = false
+	animation_player.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished(animation_name: String):
-    if animation_name == "fade_in":
-        on_transition_finished.emit()
-        animation_player.play("fade_out")
-    elif animation_name == "fade_out":
-        color_rect.visible = false
+	if animation_name == "fade_in":
+		on_transition_finished.emit()
+		animation_player.play("fade_out")
+	elif animation_name == "fade_out":
+		panel.visible = false
 
 func transition():
-    color_rect.visible = true
-    animation_player.play("fade_in")
+	panel.visible = true
+	animation_player.play("fade_in")
