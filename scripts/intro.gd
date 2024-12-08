e6682171fee370c37d85e77210d85d89789d26a3
marcovcode <marcov.code@gmail.com>
2024-12-08 19:31:47 +0100

    Implement intro dialogue

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
 func _ready() -> void:
+    Dialogic.signal_event.connect(_on_dialogic_signal)
     Dialogic.start("intro")
+
+func _on_dialogic_signal(argument: String) -> void:
+    if argument == "disable_user_input":
+        Dialogic.Inputs.block_input(9999)
