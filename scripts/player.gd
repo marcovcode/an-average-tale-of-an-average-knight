6f7e357c80c15fa1fc2ae0d62372838c9ba134c5
marcovcode <marcov.code@gmail.com>
2024-12-09 18:44:44 +0100

    Make player should go to witch dialogue

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 80
+var can_move = true
 var last_direction = "down"
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -11,11 +12,15 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	input_direction = input_direction.normalized()
 
-	velocity = input_direction * SPEED
+	if can_move:
+		velocity = input_direction * SPEED
 
 	return input_direction
 
 func animate(input_direction):
+	if !can_move:
+		return
+
 	is_moving = input_direction != Vector2.ZERO
 
 	if is_moving:
@@ -46,6 +51,17 @@ func animate(input_direction):
 			animated_sprite.play("idle_down")
 		elif last_direction == "up":
 			animated_sprite.play("idle_up")
+
+func _on_dialogic_signal(argument: String) -> void:
+	match argument:
+		"disable_player_movement":
+			can_move = false
+		"enable_player_movement":
+			can_move = true
+
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.start("player_should_go_to_witch")
 
 func _physics_process(delta):
 	var input_direction = get_input()
