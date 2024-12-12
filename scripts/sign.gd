18c93da3a806492765382cb57159fb913529f44e
marcovcode <marcov.code@gmail.com>
2024-12-12 18:31:55 +0100

    Fix sign interact

--- scripts/sign.gd
+++ scripts/sign.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends StaticBody2D
 
 @export var timeline = ""
 @onready var player = %Player
@@ -6,12 +6,16 @@ extends Area2D
 var is_talking = false
 
 func _ready() -> void:
-	Dialogic.timeline_ended.connect(_on_timeline_ended)
+    Dialogic.timeline_ended.connect(_on_timeline_ended)
 
 func _process(delta):
-	if player.get_node("RayCast2D").is_colliding() and Input.is_action_just_pressed("interact") and not is_talking:
-		is_talking = true
-		Dialogic.start(timeline)
+    var raycast = player.get_node("RayCast2D")
+    
+    if raycast.is_colliding():
+        var collided_object = raycast.get_collider()
+        if collided_object == self and Input.is_action_just_pressed("interact") and not is_talking:
+            is_talking = true
+            Dialogic.start(timeline)
 
 func _on_timeline_ended():
-	is_talking = false
+    is_talking = false
