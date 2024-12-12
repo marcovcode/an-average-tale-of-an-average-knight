7a267ea79af5ad8ba650541a3b3906b3a24a01fe
marcovcode <marcov.code@gmail.com>
2024-12-12 18:04:43 +0100

    Implement sign dialogue

---
+++ scripts/sign.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var timeline = ""
+@onready var player = %Player
+
+var is_talking = false
+
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+
+func _process(delta):
+	if player.get_node("RayCast2D").is_colliding() and Input.is_action_just_pressed("interact") and not is_talking:
+		is_talking = true
+		Dialogic.start(timeline)
+
+func _on_dialogic_signal(argument):
+	if argument == "dialogue_ended":
+		is_talking = false
